093795c4b075af4e8fbf95e176825774a4ef9bdc
Jeyson Gonzalez <jsgon8@gmail.com>
2022-08-20 19:53:40 -0500

    feat(home): add home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Home', url: '/folder/Inbox', icon: 'home' },
+    { title: 'Places', url: '/folder/Outbox', icon: 'heart' },
+    { title: 'Hotels', url: '/folder/Favorites', icon: 'bed' },
+    { title: 'Restaurants', url: '/folder/Archived', icon: 'pizza' },
+    { title: 'Guides', url: '/folder/Trash', icon: 'person' },
+    { title: 'Transport', url: '/folder/Spam', icon: 'car' },
+    { title: 'Home', url: '/home', icon: 'home' },
+    { title: 'Customers', url: '/customers', icon: 'person' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
